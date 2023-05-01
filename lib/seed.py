2b7a3a6c80c602fc32ae65409cff210bfdb239d9
Enoch2k2 <enoch2k2@hotmail.com>
2023-05-01 14:01:28 -0500

    finished

--- lib/seed.py
+++ lib/seed.py
@@ -10,7 +10,30 @@ from models import Game
 fake = Faker()
 
 if __name__ == '__main__':
-    
+
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+session.query(Game).delete()
+session.commit()
+
+# botw = Game(title="Breath of the Wild", platform="Switch",
+#             genre="Adventure", price=60)
+# ffvii = Game(title="Final Fantasy VII",
+#              platform="Playstation", genre="RPG", price=30)
+# mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+# grounded = Game(title="Grounded", platform="PC", genre="Survival", price=30)
+
+games = [
+    Game(
+        title=fake.name(),
+        platform=fake.word(),
+        genre=fake.word(),
+        price=random.randint(0, 60)
+    )
+    for i in range(50)
+]
+
+session.add_all(games)
+session.commit()
